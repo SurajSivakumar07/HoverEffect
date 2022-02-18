454829dda5ac0a0c5d091abb199b5009e6d28f00
Surajsuri07 <96371899+Surajsuri07@users.noreply.github.com>
2022-02-18 20:20:44 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,62 @@
+//Selecting Elementss
+
+
+let red=document.getElementById("red")
+let orange=document.getElementById("orange")
+let yellow=document.getElementById("yellow")
+let green=document.getElementById("green")
+let blue=document.getElementById("blue")
+let indigo=document.getElementById("indigo")
+let violet=document.getElementById("violet")
+
+//For red
+red.addEventListener("mousemove",function(){
+  
+    document.body.style.backgroundColor="red"
+   
+})
+red.addEventListener("mouseleave",function(){
+    document.body.style.backgroundColor="white"
+})
+//For Orange
+orange.addEventListener("mousemove",function(){
+    document.body.style.backgroundColor="orange"
+})
+orange.addEventListener("mouseleave",function(){
+    document.body.style.backgroundColor="white"
+})
+//FOr Yellow
+yellow.addEventListener("mousemove",function(){
+    document.body.style.backgroundColor="yellow"
+})
+yellow.addEventListener("mouseleave",function(){
+    document.body.style.backgroundColor="white"
+})
+//For green
+green.addEventListener("mousemove",function(){
+    document.body.style.backgroundColor="green"
+})
+green.addEventListener("mouseleave",function(){
+    document.body.style.backgroundColor="white"
+})
+//For blue
+blue.addEventListener("mousemove",function(){
+    document.body.style.backgroundColor="blue"
+})
+blue.addEventListener("mouseleave",function(){
+    document.body.style.backgroundColor="white"
+})
+//For indigo
+indigo.addEventListener("mousemove",function(){
+    document.body.style.backgroundColor="indigo"
+})
+indigo.addEventListener("mouseleave",function(){
+    document.body.style.backgroundColor="white"
+})
+//For violet
+violet.addEventListener("mousemove",function(){
+    document.body.style.backgroundColor="violet"
+})
+violet.addEventListener("mouseleave",function(){
+    document.body.style.backgroundColor="white"
+})
